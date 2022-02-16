c9303231a432e707f698f325e0e74c2d50d3191c
fengyongkang <wp06894@1.cn>
2022-02-16 10:47:58 +0800

    项目刚拉下来，解决启动报错

--- src/main/java/com/yeqifu/sys/controller/LoginfoController.java
+++ src/main/java/com/yeqifu/sys/controller/LoginfoController.java
@@ -4,7 +4,6 @@ package com.yeqifu.sys.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.sun.org.apache.regexp.internal.RE;
 import com.yeqifu.sys.common.DataGridView;
 import com.yeqifu.sys.common.ResultObj;
 import com.yeqifu.sys.entity.Loginfo;
@@ -13,7 +12,6 @@ import com.yeqifu.sys.vo.LoginfoVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
